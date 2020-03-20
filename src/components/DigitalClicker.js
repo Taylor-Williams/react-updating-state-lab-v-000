35b9dc8daf96ced59c8eac06977f2a31f75459b7
Taylor Williams <taylor@thewilliams.net>
2020-03-20 21:50:46 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,17 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+class DigitalClicker extends React.Component {
+
+  constructor() {
+    
+  }
+  clicked = () => {
+    this.setState((previousState) => {
+      return {timesClicked: this.previousState.timesClicked++}
+    })
+  }
+  render(){
+    return <button onClick={this.clicked}>clicked {this.state.timesClicked} times!</button>
+  }
+}
+export default DigitalClicker
